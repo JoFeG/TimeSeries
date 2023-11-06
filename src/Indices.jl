89044a41d9271303d9f139c51af598f8489bb6b2
JoFeG <jose.fernandez.g@usach.cl>
2023-11-06 11:46:35 -0300

    Gamma Index

--- src/Indices.jl
+++ src/Indices.jl
@@ -30,3 +30,38 @@ function dunnindex(
     DI = minimum(δ) / maximum(Δ)
     return DI
 end
+
+
+function gammaindex(
+        assignments::Vector{<:Int},
+        dist::Matrix{<:Real}
+    )
+    issymmetric(dist) || throw(ArgumentError("Distance Matrix is not symmetric"))
+    
+    n = length(assignments)
+    n == size(dist)[1] || throw(DimensionMismatch("Distance Matrix and assignments dimension mismatch"))
+    
+    k = length(unique(assignments))
+    1:k == unique(assignments) || throw(ArgumentError("assignments vector is not of consecutive integer values"))
+    
+    s_pluss = 0
+    s_minus = 0
+    for i = 1:n
+        for j = i+1:n
+            for p=1:n 
+                for q=p+1:n
+                    if assignments[p] == assignments[q]  && assignments[i] != assignments[j]
+                        if dist[i,j] > dist[p,q]
+                            s_pluss = s_pluss + 1 
+                        elseif dist[i,j] < dist[p,q]
+                            s_minus = s_minus + 1
+                        end
+                    end
+                end
+            end
+        end
+    end
+    
+    ΓI = (s_pluss - s_minus) / (s_pluss + s_minus)
+    return ΓI     
+end
