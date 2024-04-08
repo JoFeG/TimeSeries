9d88fbf9990e64c397eb5ccb2b909f2ee7ea9516
jose.fernandez.g <jose.fernandez.g@usach.cl>
2024-04-08 09:59:15 -0300

    paper plots

--- test/plots_UCRA_indices.jl
+++ test/plots_UCRA_indices.jl
@@ -54,3 +54,84 @@ bar!(
 )
 
 savefig(fig, "figs/all_suc_rate_means.svg")
+
+##################################################################
+
+algs_suc_rate_means = zeros(5,2)
+
+for i = 1:5
+    sum_i1 = 0
+    sum_i2 = 0
+    for s = 1:2
+        for d = 1:4
+            col1 = join([alg[1], index[i], set[s], dist[d]],"_")
+            sum_i1 = sum_i1 + mean(Indices_df[!, col1])
+            col2 = join([alg[2], index[i], set[s], dist[d]],"_")
+            sum_i2 = sum_i2 + mean(Indices_df[!, col2])
+        end
+    end
+    algs_suc_rate_means[i,1] = sum_i1 / (2*4)
+    algs_suc_rate_means[i,2] = sum_i2 / (2*4)
+end
+
+fig = plot(
+    size = (310,220),
+    grid = :y
+)
+
+fo_y = font(8, family="sans-serif")
+fo_x = font(12, family="sans-serif")
+perm = sortperm(algs_suc_rate_means[:,1], rev=true)
+groupedbar!(
+    index_labels[perm], 
+    [algs_suc_rate_means[perm,1] algs_suc_rate_means[perm,2]],
+    label = [alg[1] alg[2]],
+    yguidefont = fo_y,
+    ylabel = "Proportion of success",
+    ytickfont = fo_y,
+    yrotation = 90,
+    xtickfont = fo_x
+)
+
+savefig(fig, "figs/algs_suc_rate_means.svg")
+
+##################################################################
+
+set_suc_rate_means = zeros(5,2)
+
+for i = 1:5
+    sum_i1 = 0
+    sum_i2 = 0
+    for a = 1:2
+        for d = 1:4
+            col1 = join([alg[a], index[i], set[1], dist[d]],"_")
+            sum_i1 = sum_i1 + mean(Indices_df[!, col1])
+            col2 = join([alg[a], index[i], set[2], dist[d]],"_")
+            sum_i2 = sum_i2 + mean(Indices_df[!, col2])
+        end
+    end
+    set_suc_rate_means[i,1] = sum_i1 / (2*4)
+    set_suc_rate_means[i,2] = sum_i2 / (2*4)
+end
+
+fig = plot(
+    size = (310,220),
+    grid = :y
+)
+
+fo_y = font(8, family="sans-serif")
+fo_x = font(12, family="sans-serif")
+perm = sortperm(set_suc_rate_means[:,1], rev=true)
+groupedbar!(
+    index_labels[perm], 
+    [set_suc_rate_means[perm,1] set_suc_rate_means[perm,2]],
+    label = [set[1] set[2]],
+    yguidefont = fo_y,
+    ylabel = "Proportion of success",
+    ytickfont = fo_y,
+    yrotation = 90,
+    xtickfont = fo_x
+)
+
+savefig(fig, "figs/set_suc_rate_means.svg")
+
