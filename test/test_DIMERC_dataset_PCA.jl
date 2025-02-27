3e84996bc6349b330e28a2084181657de4907e93
JoFeGo <jose.fernandez.g@usach.cl>
2025-02-27 08:44:12 -0300

    DIMERC size plots

--- test/test_DIMERC_dataset_PCA.jl
+++ test/test_DIMERC_dataset_PCA.jl
@@ -15,39 +15,20 @@ fig = plot(
     grid = :y
     )
 
-scatter!(F.S)
-#savefig(fig, savepath*"test.pdf")
-#display(fig)
+##### Singular values plot
 
+# scatter!(F.S)
+# scatter!(log.(F.S))
+# savefig(fig, savepath*"test.pdf")
+# display(fig)
+
+##### Projection to dimension k=2
+
+k = 2
 Σ = Diagonal(F.S)
-
-k=2
 T = Σ[1:k,1:k] * F.Vt[1:k,:]
 
-m,n=size(Y)
-Gap_lengths = zeros(m)
-Gap_cap = 104
-ids_over_cap = zeros(Int,n)
-for k = 1:n
-    count = 0
-    for i = 1:m
-        if Y[i,k] == 0
-            count += 1
-        else
-            if count > 0
-                Gap_lengths[count] += 1
-                count >= Gap_cap ? ids_over_cap[k] = 1 : nothing
-            end
-            count = 0
-        end
-    end
-    if count > 0
-        Gap_lengths[count] += 1
-        count >= Gap_cap ? ids_over_cap[k] = 1 : nothing
-    end
-end
-
-
+m, n = size(Y)
 
 norms = norm.(eachcol(Y))
 colors = zeros(n)
@@ -72,6 +53,7 @@ scatter!(
 #display(fig)
 
 
+
 fig = plot(
     margin=20pt,
     size = (2*600,2*220), 
@@ -82,6 +64,8 @@ scatter!(sort(norm.(eachcol(Y))))
 #savefig(fig, savepath*"test.pdf")
 #display(fig)
 
+
+#=
 
 YY = Y[:,(norms.>1e2).&(norms.<1e3)]
 
@@ -116,3 +100,4 @@ scatter!(
 )
 
 display(fig)
+=#
---
+++ test/test_DIMERC_dataset_transformation.jl
@@ -0,0 +1,86 @@
+using LinearAlgebra
+using Plots
+using Statistics
+
+include("../src/DIMERC_utils.jl")
+
+json = LoadDataSet_DIMERC_json()
+Y = DIMERCseriesjson2matrix(json)
+
+include("./test_DIMERC_dataset_zeros_handling.jl")
+
+m, nn = size(YY)
+
+#=
+X = norm.(eachcol(Y))
+norms = sort(X, rev=true)
+
+color = ((ids_over_max_gap + ids_over_max_tot) .!= 0) .+ 1
+color = color[sortperm(X, rev=true)]
+
+fig = plot(
+    margin=20pt,
+    size = (2*600,2*220), 
+    grid = :y,
+    ylabel = "Euclidean Norm",
+    xlabel = "Series"
+    )
+
+bar!(log10.(norms),
+    linecolor = nothing,
+    color = color,
+        yticks = (
+        [   
+            log10.(1:9)...,
+            log10.(10:10:90)...,
+            log10.(100:100:900)...,
+            log10.(1000:1000:9000)...,
+            log10.(10000:10000:90000)...,
+            log10.(100000:100000:900000)...,
+            log10.(1000000)
+        ],[
+            "1",
+            ["" for i=1:8]...,
+            "10",
+            ["" for i=1:8]...,
+            "100",
+            ["" for i=1:8]...,
+            "1000",
+            ["" for i=1:8]...,
+            "10000",
+            ["" for i=1:8]...,
+            "100000",
+            ["" for i=1:8]...,
+            "1000000"
+        ]),
+    label = "Series not excluded by zeros caps"
+)
+
+savefig(fig, "figs/DIMERC_data_norms.pdf")
+
+=#
+
+for k = 1:nn
+    y = YY[:, k]
+    a = minimum(y)
+    b = maximum(y) - a 
+    YY[:, k] = (y .- a) ./ b
+end
+
+
+norms = sort(norm.(eachcol(YY)), rev=true)
+
+fig = plot(
+    margin=20pt,
+    size = (2*600,2*220), 
+    grid = :y,
+    ylabel = "Euclidean Norm",
+    xlabel = "Series"
+    )
+
+bar!(norms,
+    linecolor = nothing,
+    label = "Series not excluded by zeros caps"
+)
+
+savefig(fig, "figs/DIMERC_data_norms_after.pdf")
