46499fb9f403526f6f409ed462e190dc96cb4f2d
JoFeGo <jose.fernandez.g@usach.cl>
2025-03-05 11:56:32 -0300

    FREQ utils

--- src/FREQ_utils.jl
+++ src/FREQ_utils.jl
@@ -1,10 +1,7 @@
 using DelimitedFiles
 
 function LoadSeries_FREQ(path)
-    println(path)
     data, header = readdlm(path, ',', header=true)
-    display(header)
-    display(data)
     
     records = [occursin("CL_",h) for h in header]
     M = size(data)[1]
@@ -15,5 +12,23 @@ function LoadSeries_FREQ(path)
         end
     end
     y_raw / sum(records)
+    
+    ## OBS y_raw == data[:,findfirst(i -> i == "CIO", header)[2]]
     return y_raw
+end
+
+function SamplerRaw_FREQ(path; Ts=0.3, event_time=0, f0=50)
+    data, header = readdlm(path, ',', header=true)
+    
+    y_raw = data[:,findfirst(i -> i == "CIO", header)[2]]
+    
+    # WARNING: This assumes 0.02 sampling time in the raw file!
+    delta_i = floor(Int, Ts / 0.02)
+    index = 1:delta_i:length(y_raw)
+    
+    delta_freq = y_raw[index] .- f0
+    P0 = data[:,findfirst(i -> i == "Potencia", header)[2]][1]
+    time = Ts * (0:length(delta_freq)-1)
+    
+    return delta_freq, P0, time
 end
--- test/test_FREQ.jl
+++ test/test_FREQ.jl
@@ -9,5 +9,5 @@ summary, header = readdlm("./data/freq/experiment_data_summary.csv", ',', header
 summary
 
 i = 3
-y_raw = LoadSeries_FREQ(path * summary[i,1] * ".csv")
+y, P0, t = SamplerRaw_FREQ(path * summary[i,1] * ".csv")
 
