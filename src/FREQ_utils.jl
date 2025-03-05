2b6014f8c9506abe76af146e1b3767a89c4a7025
JoFeGo <jose.fernandez.g@usach.cl>
2025-03-05 16:56:40 -0300

    FREQ test

--- src/FREQ_utils.jl
+++ src/FREQ_utils.jl
@@ -17,15 +17,24 @@ function LoadSeries_FREQ(path)
     return y_raw
 end
 
-function SamplerRaw_FREQ(path; Ts=0.3, event_time=0, f0=50)
+function SamplerRaw_FREQ(path; Ts=0.3, event_time=30, f0=50)
     data, header = readdlm(path, ',', header=true)
     
     y_raw = data[:,findfirst(i -> i == "CIO", header)[2]]
     
     # WARNING: This assumes 0.02 sampling time in the raw file!
     delta_i = floor(Int, Ts / 0.02)
-    index = 1:delta_i:length(y_raw)
+    start = data[:,findfirst(i -> i == "Inicio", header)[2]][1]
     
+    index = start:delta_i:(start + floor(Int, event_time / 0.02))
+#=
+    println("
+start      = $start
+index[1]   = $(index[1])
+delta_i    = $delta_i          
+index[end] = $(index[end])
+        ")
+=#
     delta_freq = y_raw[index] .- f0
     P0 = data[:,findfirst(i -> i == "Potencia", header)[2]][1]
     time = Ts * (0:length(delta_freq)-1)
--- test/test_FREQ.jl
+++ test/test_FREQ.jl
@@ -1,13 +1,32 @@
 include("../src/FREQ_utils.jl")
-
 path = "./data/freq/raw/2023_set_n1/"
-
 
 using DelimitedFiles
 summary, header = readdlm("./data/freq/experiment_data_summary.csv", ',', header=true)
 
-summary
+n = size(summary)[1]
+m = 101
 
-i = 3
-y, P0, t = SamplerRaw_FREQ(path * summary[i,1] * ".csv")
+Y  = zeros(n,m)
+P0 = zeros(n) 
+for i = 1:n
+    y, p0, t = SamplerRaw_FREQ(path * summary[i,1] * ".csv")
+    P0[i] = p0
+    Y[i,:] = y
+end
 
+using Plots
+using Measures
+
+fig = plot(
+    margin = 20pt,
+    size = (900,440), 
+    grid = :y,
+    xticks = 1:30,
+    xlabel = "Time [seg]",
+    ylabel = "Δf [Hz]"
+)
+
+for i = 1:n
+    plot!(t,Y[i,:], label=false, color=1)
+end
